378fcb1f280d871fbee8535bc5aeab8dfbcb1445
greenteawarrior <emily.wang@students.olin.edu>
2014-03-10 19:27:04 -0400

    woot! works on api16  we can send strings from android to node.js server on laptop :-)

--- UDPSender_Emily/app/src/main/java/fakecompany/udpsender/app/MainActivity.java
+++ UDPSender_Emily/app/src/main/java/fakecompany/udpsender/app/MainActivity.java
@@ -1,9 +1,11 @@
 package fakecompany.udpsender.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.send_message).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new UDPSender()).start();
+            }
+        });
     }
 
 
---
+++ UDPSender_Emily/app/src/main/java/fakecompany/udpsender/app/UDPSender.java
@@ -0,0 +1,55 @@
+package fakecompany.udpsender.app;
+
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by greenteawarrior on 3/10/14.
+ */
+
+public class UDPSender implements Runnable{
+    private DatagramSocket socket;
+    private MediaRecorder recorder;
+    private String address_string = "192.168.51.219"; //use ip addr command in linux
+    private int foreign_port = 8888;
+    private int local_port = 8888;
+    private InetAddress foreign_address;
+
+    @Override
+    public void run() {
+        try {
+            foreign_address  = InetAddress.getByName(address_string);
+            socket = new DatagramSocket(local_port);
+            String message = "hello world";
+            byte[] data = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(data, data.length, foreign_address, foreign_port);
+            socket.send(packet);
+            Log.d("UDP", "trying to hello world");
+            socket.close();
+            Log.d("UDP", "socket closed");
+        } catch (Exception e) {
+            e.printStackTrace();
+            cleanShutdown();
+        }
+    }
+
+
+    private void sendTestPacket() throws IOException {
+        String message = "hello world";
+        byte[] data = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length, foreign_address, foreign_port);
+        socket.send(packet);
+    }
+
+    private void cleanShutdown() {
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
+}
