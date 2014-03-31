df369532e89f0469dd255e43cc4aef37dbc393bc
Cypress Frankenfeld <iamcypress@gmail.com>
2014-03-30 20:44:40 -0400

    added test message

--- CameraDemo/app/src/main/java/fakecompany/udpsender/camerademo/app/VideoStreamer.java
+++ CameraDemo/app/src/main/java/fakecompany/udpsender/camerademo/app/VideoStreamer.java
@@ -2,11 +2,23 @@ package fakecompany.udpsender.camerademo.app;
 
 import android.util.Log;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  * Created by cypressf on 3/30/14.
  */
 public class VideoStreamer implements Runnable {
 
+    // initializing the things here so they're accessible by all the functions in this class :-)
+    private DatagramSocket socket;
+//    private MediaRecorder recorder;
+    private String address_string = "10.7.88.20";
+    private int foreign_port = 8888;
+    private int local_port = 8888;
+    private InetAddress foreign_address;
     private MainActivity activity;
 
     public VideoStreamer(MainActivity activity) {
@@ -15,6 +27,48 @@ public class VideoStreamer implements Runnable {
 
     @Override
     public void run() {
-        Log.d(MainActivity.TAG, "starting thread (and stopping it. it's a short thread)");
+        Log.d(MainActivity.TAG, "starting thread");
+        try {
+            // Set up the socket things
+            foreign_address  = InetAddress.getByName(address_string);
+            socket = new DatagramSocket(local_port);
+        } catch (Exception e) {
+            e.printStackTrace();
+            cleanShutdown();
+        }
+        sendTestPacket();
+        cleanShutdown();
+//        startRecording();
+    }
+
+    private void sendTestPacket() {
+        //Seeing if the android to webapp communication worked with a "hello world" string. Woot!
+        String message = "hello world";
+        byte[] data = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length, foreign_address, foreign_port);
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void cleanShutdown() {
+        //A convenient function to tie up loose ends when testing/running the app.
+        //Takes care of the recorder, camera, and sockets.
+
+//        if (recorder != null) {
+//            recorder.stop();
+//            recorder.reset();   // clear recorder configuration
+//            recorder.release(); // release the recorder object
+//            recorder = null;
+//        }
+//        if (activity.camera != null) {
+//            activity.camera.stopPreview();
+//            activity.camera.release();
+//        }
+        if (socket != null) {
+            socket.close();
+        }
     }
 }
